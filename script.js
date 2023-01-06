ca7760fdd5c2680d2a48cee518e9d5449245a902
Eric Graves <graves.eric@gmail.com>
2023-01-06 15:28:12 -0600

    added JS questions and fixed answer button functions

--- script.js
+++ script.js
@@ -4,13 +4,22 @@ let qNum = 0
 let secondsLeft = 20
 let timerInterval
 let initials = ""
-const q1 = ["Why did the chicken cross the road?", "To smell the roses.", "To find the lost puppy.", "To get to the other side.", "To escape certain death.", 3]
-const q2 = ["Mary had a...?", "Little Lamb", "Tiny Turtle", "Silly Snake", "Brown Bird", 1]
-const q3 = ["Vincent van Gogh cut off his?", "Finger", "Ear", "Nose", "Foot", 2]
-const q4 = ["What is the color of an emerald?", "Red", "Yellow", "Blue", "Green", 4]
+const q1 = ["How to write an IF statement in JavaScript?", "if i===5 then", "if i = 5", "if (i==5)", "if i=5 then", 3]
+const q2 = ["How to write an IF statement for executing some code if i is NOT equal to 5?", "if (i!=5)", "if (i<>5)", "if i=!5 then", "if i<>5", 1]
+const q3 = ["How do you create a function in JavaScript?", "function = myFunction()", "function myfunction()", "function:myFunction()", "function-myFunction()", 2]
+const q4 = ["How do you call a function named myFunction?", "call myFunction()", "callfunction myFunction()", "call.myFunction()", "myFunction()", 4]
+
+// const q2 = {
+//     q: "Why did the chicken cross the road?",
+//     1: "To smell the roses.",
+//     2: "To find the lost puppy.",
+//     3: "To get to the other side.",
+//     4: "To escape certain death.",
+//     a: 3
+// }
 
 var questions = [q1, q2, q3, q4]
-var array = [].concat(questions);
+var pool = [].concat(questions);
 
 document.getElementById("timer").innerHTML = secondsLeft;
 document.getElementById("strBtn").addEventListener("click", startQuiz);
@@ -31,15 +40,15 @@ function reset() {
 //     rando = questArr[Math.floor(Math.random() * questArr.length)];
 // }
 
-function shuffle(array) {
-    var m = array.length, t, i;
+function shuffle() {
+    var m = pool.length, t, i;
     while (m) {
         i = Math.floor(Math.random() * m--);
-        t = array[m];
-        array[m] = array[i];
-        array[i] = t;
+        t = pool[m];
+        pool[m] = pool[i];
+        pool[i] = t;
     }
-    return array;
+    return pool;
 }
 
 function startTimer() {
@@ -59,16 +68,16 @@ function startTimer() {
 
 function startQuiz() {
     startTimer()
-    console.log("Before: " + array[0])
-    shuffle(array)
-    console.log("After: " + array[0])
-    rAns = array[qNum][5]
+    console.log("Before: " + pool[0])
+    shuffle(pool)
+    console.log("After: " + pool[0])
+    rAns = pool[qNum][5]
     console.log("Correct Answer: " + rAns)
-    document.getElementById("question").innerHTML = array[qNum][0];
-    document.getElementById("ansBtn1").innerHTML = array[qNum][1];
-    document.getElementById("ansBtn2").innerHTML = array[qNum][2];
-    document.getElementById("ansBtn3").innerHTML = array[qNum][3];
-    document.getElementById("ansBtn4").innerHTML = array[qNum][4];
+    document.getElementById("question").innerHTML = pool[qNum][0];
+    document.getElementById("ansBtn1").innerHTML = pool[qNum][1];
+    document.getElementById("ansBtn2").innerHTML = pool[qNum][2];
+    document.getElementById("ansBtn3").innerHTML = pool[qNum][3];
+    document.getElementById("ansBtn4").innerHTML = pool[qNum][4];
     document.getElementById("ansBtn1").addEventListener("click", ans1);
     document.getElementById("ansBtn2").addEventListener("click", ans2);
     document.getElementById("ansBtn3").addEventListener("click", ans3);
@@ -77,14 +86,14 @@ function startQuiz() {
 
 function nextQuestion() {
     if (qNum < 4) {
-        console.log("question: " + array[qNum])
-        rAns = array[qNum][5]
+        console.log("question: " + pool[qNum])
+        rAns = pool[qNum][5]
         console.log("Correct Answer: " + rAns)
-        document.getElementById("question").innerHTML = array[qNum][0];
-        document.getElementById("ansBtn1").innerHTML = array[qNum][1];
-        document.getElementById("ansBtn2").innerHTML = array[qNum][2];
-        document.getElementById("ansBtn3").innerHTML = array[qNum][3];
-        document.getElementById("ansBtn4").innerHTML = array[qNum][4];
+        document.getElementById("question").innerHTML = pool[qNum][0];
+        document.getElementById("ansBtn1").innerHTML = pool[qNum][1];
+        document.getElementById("ansBtn2").innerHTML = pool[qNum][2];
+        document.getElementById("ansBtn3").innerHTML = pool[qNum][3];
+        document.getElementById("ansBtn4").innerHTML = pool[qNum][4];
         document.getElementById("ansBtn1").addEventListener("click", ans1);
         document.getElementById("ansBtn2").addEventListener("click", ans2);
         document.getElementById("ansBtn3").addEventListener("click", ans3);
@@ -92,7 +101,9 @@ function nextQuestion() {
     } else {
         setScore = secondsLeft;
         initials = prompt("COMPLETED! You scored: " + setScore + " Enter your initials for the scoreboard")
-        console.log(initials)
+        var node = document.createElement('li');
+        node.appendChild(document.createTextNode(initials));
+        document.querySelector('ul').appendChild(node);
         reset()
     }
 
@@ -124,8 +135,17 @@ function ans2() {
     console.log("Comparing " + rAns + " to " + uAns)
     if (uAns == rAns) {
         alert("Correct!")
+        qNum++
+        console.log(qNum)
+        nextQuestion()
     } else {
-        alert("Wrong!")
+        alert("Incorrect! You have lost 3 seconds of time!")
+        qNum++
+        secondsLeft--
+        secondsLeft--
+        secondsLeft--
+        nextQuestion()
+        console.log(qNum)
     }
 }
 
@@ -135,8 +155,17 @@ function ans3() {
     console.log("Comparing " + rAns + " to " + uAns)
     if (uAns == rAns) {
         alert("Correct!")
+        qNum++
+        console.log(qNum)
+        nextQuestion()
     } else {
-        alert("Wrong!")
+        alert("Incorrect! You have lost 3 seconds of time!")
+        qNum++
+        secondsLeft--
+        secondsLeft--
+        secondsLeft--
+        nextQuestion()
+        console.log(qNum)
     }
 }
 
@@ -146,11 +175,19 @@ function ans4() {
     console.log("Comparing " + rAns + " to " + uAns)
     if (uAns == rAns) {
         alert("Correct!")
+        qNum++
+        console.log(qNum)
+        nextQuestion()
     } else {
-        alert("Wrong!")
+        alert("Incorrect! You have lost 3 seconds of time!")
+        qNum++
+        secondsLeft--
+        secondsLeft--
+        secondsLeft--
+        nextQuestion()
+        console.log(qNum)
     }
 }
-
 
 
 
